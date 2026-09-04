bf7b5e2fba2327ca13fe9a5f5fae2b236f502d89
Achehri-Hassan <hachehri@gmail.com>
2026-09-04 14:23:37 +0200

    create responsve navbar and add dark mood

---
+++ src/components/ThemeTogglebtn.jsx
@@ -0,0 +1,37 @@
+
+
+
+import React, { useEffect } from 'react'
+import assets from '../assets/assets'
+
+function ThemeTogglebtn({theme , setTheme}) {
+  
+  useEffect(()=>{
+      const prefersDarkMode = window.matchMedia('(perfers-color-scheme: dark)').matches;
+      setTheme(theme || (prefersDarkMode ? 'dark' : 'light'))
+  },[])
+
+  useEffect(()=> {
+      if(theme === "dark"){
+          document.documentElement.classList.add("dark")
+      }else{
+         document.documentElement.classList.remove("dark")
+      }
+      localStorage.setItem('theme' ,theme)
+  } , [theme])
+
+  return (
+    
+      <>
+        <button>
+            {theme === 'dark'  ? (
+               <img  onClick={() => setTheme('light')} src={assets.sun_icon} alt="sun_icon" className='size-8.5 p-1.5 border border-gray-500 rounded-full' />
+            ) : (
+               <img onClick={() => setTheme('dark')} src={assets.moon_icon}  alt="moon-icon" className='size-8.5 p-1.5 border border-gray-500 rounded-full' />
+            ) }
+        </button>
+      </>
+  )
+}
+
+export default ThemeTogglebtn
